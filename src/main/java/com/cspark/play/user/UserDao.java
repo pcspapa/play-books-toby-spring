9444ef76f1e8eeb890baa5a5274c15fdad563ad2
Chanseok Park <pcspapa@inzido.co.kr>
2021-06-07 18:28:11 +0900

    feat: chapter.1.3

--- src/main/java/com/cspark/play/user/UserDao.java
+++ src/main/java/com/cspark/play/user/UserDao.java
@@ -1,16 +1,20 @@
 package com.cspark.play.user;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
 
+  private final ConnectionMaker connectionMaker;
+
+  public UserDao(ConnectionMaker connectionMaker) {
+    this.connectionMaker = connectionMaker;
+  }
+
   public void add(User user) throws ClassNotFoundException, SQLException {
-    Class.forName("org.h2.Driver");
-    Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby-spring", "sa", "");
+    Connection conn = connectionMaker.getConnection();
 
     PreparedStatement ps = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
     ps.setString(1, user.getId());
@@ -24,8 +28,7 @@ public class UserDao {
   }
 
   public User get(String id) throws ClassNotFoundException, SQLException {
-    Class.forName("org.h2.Driver");
-    Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby-spring", "sa", "");
+    Connection conn = connectionMaker.getConnection();
 
     PreparedStatement ps = conn.prepareStatement("select * from users where id = ?");
     ps.setString(1, id);
@@ -33,10 +36,17 @@ public class UserDao {
     ResultSet rs = ps.executeQuery();
     rs.next();
 
-    return new User(
+    User findUser = new User(
         rs.getString("id"),
         rs.getString("name"),
         rs.getString("password")
     );
+
+    rs.close();
+    ps.close();
+    conn.close();
+
+    return findUser;
   }
+
 }
