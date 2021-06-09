2d7140f02aab8436659315188a386f1acf18c0a8
Chanseok Park <pcspapa@inzido.co.kr>
2021-06-09 10:11:38 +0900

    feat: chapter.1.5

--- src/main/java/com/cspark/play/user/DaoFactory.java
+++ src/main/java/com/cspark/play/user/DaoFactory.java
@@ -1,14 +1,17 @@
 package com.cspark.play.user;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
-  public DaoFactory() {
-  }
-
+  @Bean
   UserDao userDDao() {
     return new UserDao(new DConnectionMaker());
   }
 
+  @Bean
   UserDao userNDao() {
     return new UserDao(new NConnectionMaker());
   }
--- src/test/java/com/cspark/play/user/UserDaoTest.java
+++ src/test/java/com/cspark/play/user/UserDaoTest.java
@@ -4,14 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class UserDaoTest {
 
-  private final DaoFactory daoFactory = new DaoFactory();
-
   @Test
   void addAndGetNUser() throws SQLException, ClassNotFoundException {  // Once?
-    UserDao dao = daoFactory.userNDao();
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userNDao", UserDao.class);
 
     dao.add(new User("mj", "Mary Jane Watson", "pw"));
 
@@ -23,7 +24,8 @@ class UserDaoTest {
 
   @Test
   void addAndGetDUser() throws SQLException, ClassNotFoundException {  // Once?
-    UserDao dao = daoFactory.userDDao();
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDDao", UserDao.class);
 
     dao.add(new User("bp", "Brad Pitt", "pw"));
 
