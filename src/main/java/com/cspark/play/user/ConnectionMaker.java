732e7b560b18413bc00778cc7a032e6719eb3614
Chanseok Park <pcspapa@inzido.co.kr>
2021-06-22 10:36:36 +0900

    refactor: chapter.3.7

--- src/main/java/com/cspark/play/user/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package com.cspark.play.user;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-
-  Connection getConnection() throws ClassNotFoundException, SQLException;
-}
--- src/test/java/com/cspark/play/user/UserDaoTest.java
+++ src/test/java/com/cspark/play/user/UserDaoTest.java
@@ -3,26 +3,17 @@ package com.cspark.play.user;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.sql.SQLException;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 @SpringBootTest
 class UserDaoTest {
 
   @Autowired
-  private ApplicationContext context;
   private UserDao dao;
-
-  @BeforeEach
-  void setUp() {
-    dao = context.getBean("userDao", UserDao.class);
-  }
 
   @Test
   void addAndGetUser() {
